06c94f91d84e40ebab23ed81f9ddeba82d053d93
Vitaliy Nikolaenko <korbendallasYP@gmail.com>
2023-09-06 21:19:56 +0300

    fix3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/unauthorized-error');
+const { UnauthorizedError } = require('../errors/unauthorized-error');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
